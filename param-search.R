d5503dcc65769e5f33085122a5887dcbf9fe528e
kbuzard <kbuzard@gmail.com>
2017-06-21 21:42:44 -0400

    presentation at CesIfo Venice 06/15/17

--- param-search.R
+++ param-search.R
@@ -2,21 +2,24 @@ T=10
 W=8
 D=5
 dl = .45
-p = .4
+p = .3
 
 #Separating w/out concessions for dh such that:
 dh_swoc = W/((1-p)*W +p*(T+D))
 
 #Separating with concessions when alpha = 1 for dh such that:
-dh_swc = W/(T+D)
+dh_stc = W/(T+D)
 
-dh = .6
+dh = dh_stc
 #Separating with concessions can be either with e=1 or e=0. Which does high type choose?
 g1 = (p*(1-dl)*(D+T))/((1-dl)*(1-p*T)+(1-p)*D) #gift necessary to separate if e = 1
 g0 = p*(D+T)                                   #gift necessary to separate if e = 0   
 
-uh_g1 = (1-dh)*-g1 + dh*(p*T*(1+g1) + p*g1 +(1-p)*(W-D-D*g1))
-uh_g0 = (1-dh)*-g0 + dh*(p*T + (1-p)*(W-D))
+uh_g1 = (1-dh)*(p-1)*g1 + dh*(p*T*(1+g1) + p*g1 +(1-p)*(W-D-D*g1))
+uh_g0 = (1-dh)*(p-1)*g0 + dh*(p*T + (1-p)*(W-D))
 
 dh1Th = W*(1-p)+1-p*T + p*(W-(T+D))/g1
 dh0Th = W*(1-p)+1-p*T + p*(W-(T+D))/g0
+
+#What would happen for these parameters with the mediator? Only dh_stc matters, 
+#and this is guaranteed by assuption on dh
